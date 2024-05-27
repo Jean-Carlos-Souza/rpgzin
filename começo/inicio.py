a0a4ca1785e25bab0f13725d5f13555453bcc79b
Jean Carlos <jeancsp2003@gmail.com>
2024-05-27 12:02:26 -0300

    update27-05

--- começo/inicio.py
+++ começo/inicio.py
@@ -3,7 +3,7 @@ from random import randint
 list_npcs = []
 
 
-def create_monster():
+def create_npcs():
     level = randint(0,50)
     new_npc = {
         'name': f'Monster #{level}',
@@ -11,8 +11,20 @@ def create_monster():
         'damage': 3 * level,
         'hp': 50 * level
     }
-    list_npcs.append(new_npc)
+    return new_npc
 
-    print(list_npcs)
 
-create_monster()
+    
+def display_npcs():
+    for npc in list_npcs:
+        print(f'Name: {npc['name']}  // Level: {npc['level']}  // Damage: {npc['damage']}  // HP: {npc['hp']}')
+
+def to_generete_npcs(n_npcs):
+
+    for x in range(n_npcs):
+        new_npc = create_npcs()
+        list_npcs.append(new_npc)
+
+
+to_generete_npcs(5)
+display_npcs()
